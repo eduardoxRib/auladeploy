daef8ce32ab99dde1e130bcff33c497515951c21
Eduardo Soares Ribeiro <eduardoxrib@gmail.com>
2019-12-04 22:16:42 -0300

    Updates

---
+++ precache-manifest.e43f38bfd15e429e860d50a7c38e3eb5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "424d33af7e7ce5579a6e62e28ca4170f",
+    "url": "/auladeploy/index.html"
+  },
+  {
+    "revision": "b9ea70384b53e4822039",
+    "url": "/auladeploy/static/css/main.aa478fa4.chunk.css"
+  },
+  {
+    "revision": "6f66dc3f1c6036b0e538",
+    "url": "/auladeploy/static/js/2.9eea3fd7.chunk.js"
+  },
+  {
+    "revision": "b9ea70384b53e4822039",
+    "url": "/auladeploy/static/js/main.68a168ac.chunk.js"
+  },
+  {
+    "revision": "b11311c64b76d62c2edd",
+    "url": "/auladeploy/static/js/runtime-main.2ce0a8e8.js"
+  },
+  {
+    "revision": "dc9fbcf06db3834ef9b43ac168ffe809",
+    "url": "/auladeploy/static/media/backIcon.dc9fbcf0.png"
+  },
+  {
+    "revision": "a81f59b90945ae39f1ba6eef6df6bfef",
+    "url": "/auladeploy/static/media/logo.a81f59b9.svg"
+  }
+]);
